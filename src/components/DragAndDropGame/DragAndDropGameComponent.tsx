f47fc69ebc808d2a03bb3b6feffa5036367ef783
MichelK <michel.kadilar@etu.univ-cotedazur.fr>
2024-06-27 12:16:24 +0700

    corrections and centerings on bin and waste for drag and drop game

--- src/components/DragAndDropGame/DragAndDropGameComponent.tsx
+++ src/components/DragAndDropGame/DragAndDropGameComponent.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import BinComponent from "./BinComponent";
 import WasteComponent from "./WasteComponent";
 import Bouteille from '../../mocks/img/bouteille.png';
 import Armoire from '../../mocks/img/armoire.png';
 import Trognon from '../../mocks/img/trognon.png';
 import PoubelleDnd from '../../mocks/img/poubelle-tri-dnd.png';
+import './DragAndDropGameComponent.css'
 
 interface WasteItem {
     name: string;
@@ -13,9 +14,9 @@ interface WasteItem {
 }
 
 const initialWasteItems: WasteItem[] = [
-    { name: 'bouteille en plastique', img: Bouteille, type: 'plastique' },
-    { name: 'armoire encombrant', img: Armoire, type: 'encombrant' },
-    { name: 'trognon alimentaire', img: Trognon, type: 'alimentaire' },
+    {name: 'bouteille en plastique', img: Bouteille, type: 'plastique'},
+    {name: 'armoire encombrant', img: Armoire, type: 'encombrant'},
+    {name: 'trognon alimentaire', img: Trognon, type: 'alimentaire'},
 ];
 
 function DragAndDropGameComponent() {
@@ -35,21 +36,24 @@ function DragAndDropGameComponent() {
     const isGameOver = wasteItems.length === 0;
 
     return (
-        <div className="App">
-            <h1>Jeu de Tri des Déchets</h1>
-            <h2>Score: {score}</h2>
+        <div className="drag-and-drop-game-container">
+            <div className="drag-and-drop-game-info">
+                <h1>Jeu de Tri des Déchets</h1>
+                <h2>Score: {score}</h2>
+            </div>
             {isGameOver ? (
-                <h2>Jeu terminé! Score final: {score}</h2>
+                <h2 className="drag-and-drop-game-final-message">Jeu terminé! Score final: {score}</h2>
             ) : (
                 <>
-                    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                        <BinComponent type="plastique" img={PoubelleDnd} onDrop={handleDrop('plastique')} />
-                        <BinComponent type="encombrant" img={PoubelleDnd} onDrop={handleDrop('encombrant')} />
-                        <BinComponent type="alimentaire" img={PoubelleDnd} onDrop={handleDrop('alimentaire')} />
+                    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                        <BinComponent type="plastique" img={PoubelleDnd} onDrop={handleDrop('plastique')}/>
+                        <BinComponent type="encombrant" img={PoubelleDnd} onDrop={handleDrop('encombrant')}/>
+                        <BinComponent type="alimentaire" img={PoubelleDnd} onDrop={handleDrop('alimentaire')}/>
                     </div>
-                    <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+                    <div style={{marginTop: '20px', display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
                         {wasteItems.map((wasteItem) => (
-                            <WasteComponent key={wasteItem.name} name={wasteItem.name} img={wasteItem.img} type={wasteItem.type} />
+                            <WasteComponent key={wasteItem.name} name={wasteItem.name} img={wasteItem.img}
+                                            type={wasteItem.type}/>
                         ))}
                     </div>
                 </>
